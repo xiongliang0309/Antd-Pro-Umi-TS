aa578306130b0e1608d86bcacadde6efcdd5afcf
熊亮 <liang.xiong@hand-china.com>
2020-09-23 16:47:31 +0800

    feat(antd): add pagination demo

--- src/pages/antd/pagination/index.tsx
+++ src/pages/antd/pagination/index.tsx
@@ -3,10 +3,25 @@ import { Pagination } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 const pagination: FC = () => {
+  function handleChange(page: number, pageSize: number | undefined): void {
+    alert(`page:${page}-pageSize:${pageSize}`);
+  }
+
+  const handleChange2 = (page: number, pageSize: number | undefined): string => {
+    alert(`page:${page}-pageSize:${pageSize}`);
+    return `page:${page}-pageSize:${pageSize}`;
+  };
+
   return (
     <PageHeaderWrapper>
-      <Pagination size="small" total={50} />
-      <Pagination size="small" total={50} showSizeChanger showQuickJumper />
+      <Pagination size="small" total={50} onChange={handleChange} />
+      <Pagination
+        size="small"
+        total={50}
+        showSizeChanger
+        showQuickJumper
+        onChange={handleChange2}
+      />
       <Pagination size="small" total={50} />
       <Pagination size="small" total={50} disabled showSizeChanger showQuickJumper />
     </PageHeaderWrapper>
